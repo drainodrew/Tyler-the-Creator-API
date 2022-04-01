860a4acadaa6780b2d8ee16b818c6e9b270cebf9
Andrew Manuel <andrewmanuel@Andrews-MacBook-Pro.local>
2022-04-01 09:36:04 -0400

    idk

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 let app = require('./app')
 let PORT = 9000
-let server = app.listen(process.env.PORT)
+let server = app.listen(PORT) //(process.env.PORT)
 
-server.on('listening', ()=> console.log("Listening on port " + process.env.PORT))
+server.on('listening', ()=> console.log("Listening on port " + PORT)) //process.env.PORT))
 server.on('error', error=> console.error("Server connection error\n", error))
