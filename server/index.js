91b2b35b4be74a5e8721679aa18fce1b9d31ba79
Andrew Manuel <andrewmanuel@Andrews-MacBook-Pro.local>
2022-04-11 11:04:57 -0400

    setting up heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 let app = require('./app')
-let PORT = 9000
-let server = app.listen(PORT) //(process.env.PORT)
+let PORT = process.env.PORT || 3000;
+let server = app.listen(PORT) 
 
-server.on('listening', ()=> console.log("Listening on port " + PORT)) //process.env.PORT))
+server.on('listening', ()=> console.log("Listening on port " + PORT))
 server.on('error', error=> console.error("Server connection error\n", error))
